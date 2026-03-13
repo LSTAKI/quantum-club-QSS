dfce11f26470984bc381d55f5f34caa79a7e1d03
TAKI ALTAF MULLA <takimulla2006@gmail.com>
2026-03-13 17:03:49 +0530

    Update grid layout for SpeakersSection component

--- src/components/SpeakersSection.tsx
+++ src/components/SpeakersSection.tsx
@@ -26,7 +26,7 @@ const SpeakersSection = () => (
         </h2>
       </div>
 
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         {speakers.map((s, i) => (
           <motion.div
             key={s.name}
